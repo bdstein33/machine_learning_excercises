c4031e5f850cadd308ad4185d78de81b6736a2c2
bdstein33 <bdstein33@gmail.com>
2016-10-16 13:58:32 -0700

    First three exercises

---
+++ machine-learning-ex2/ex2/sigmoid.m
@@ -0,0 +1,14 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid functoon
+%   J = SIGMOID(z) computes the sigmoid of z.
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+
+g = ones(size(z)) ./ (1 + e .^(z * -1));
+
+
+% =============================================================
+
+end
